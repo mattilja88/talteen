857b6edf2ed885f07dd320872978a53702a88035
mattilja88 <158097781+mattilja88@users.noreply.github.com>
2024-11-08 10:46:10 +0200

    Create MenuContext.js

---
+++ MenuContext.js
@@ -0,0 +1,30 @@
+// menu context heikin-sivu
+
+'use client';
+
+import { createContext, useState, useContext } from 'react';
+
+const MenuContext = createContext();
+
+export const MenuProvider = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <MenuContext.Provider value={{ isOpen, toggleMenu }}>
+      {children}
+    </MenuContext.Provider>
+  );
+};
+
+export const useMenu = () => {
+  const context = useContext(MenuContext);
+  if (!context) {
+    throw new Error('useMenu must be used within a MenuProvider');
+  }
+  return context;
+};
+
