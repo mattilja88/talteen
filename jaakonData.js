e0009095758db5ed482a1d993a5cc9efa452fe05
mattilja88 <158097781+mattilja88@users.noreply.github.com>
2024-11-08 10:58:07 +0200

    Create jaakonData.js

---
+++ jaakonData.js
@@ -0,0 +1,16 @@
+const data = {
+    "data_1": require('../assets/words_jsons/words_by_letter.json'),
+    "data_2": require('../assets/words_jsons/words_by_second_letter.json'),
+    "data_3": require('../assets/words_jsons/words_by_third_letter.json'),
+    "data_4": require('../assets/words_jsons/words_by_fourth_letter.json'),
+    "data_5": require('../assets/words_jsons/words_by_fifth_letter.json'),
+    "data_6": require('../assets/words_jsons/words_by_sixth_letter.json'),
+    "data_7": require('../assets/words_jsons/words_by_seventh_letter.json'),
+    "data_8": require('../assets/words_jsons/words_by_eight_letter.json'),
+    "data_9": require('../assets/words_jsons/words_by_nineth_letter.json'),
+    "data_10": require('../assets/words_jsons/words_by_tenth_letter.json'),
+    "data_11": require('../assets/words_jsons/words_by_eleventh_letter.json'),
+    "data_12": require('../assets/words_jsons/words_by_twelveth_letter.json')
+};
+
+export default data
